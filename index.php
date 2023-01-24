c48a86b58914b11f407dd2d2af731c69c2c9fb7f
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-01-24 15:24:46 +0100

    Add two php files w/ form input

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PhP Bad Words</title>
+</head>
+<body>
+
+  <main>
+    <form action="login.php" method="GET">
+      <label for="paragraph">Inerisci la tua frase:</label>
+      <input type="text" name="paragraph">
+      
+      <label for="badwords">Inserisci la parola da censurare</label>
+      <input type="text" name="badword">
+
+      <button type="submit">Invia</button>
+    </form>
+
+    <p>  </p>
+  </main>
+  
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,22 @@
+
+<h2>
+    La frase inserita è:
+
+   <?php 
+        echo $_GET["paragraph"]; 
+   ?>
+
+</h2>
+
+
+<h2>
+    La parola da censurare è:
+
+   <?php 
+        echo $_GET["badword"];
+   ?>
+   
+</h2>
+
+
+
